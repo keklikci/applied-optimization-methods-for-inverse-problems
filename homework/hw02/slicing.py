d6fda02daefa2cdc408c3fa38ae79d6aaa6edfdb
ge86yaw <kaanguney.keklikci@tum.de>
2023-05-07 15:54:02 +0200

    feat(repo): implement slicing

--- homework/hw02/slicing.py
+++ homework/hw02/slicing.py
@@ -5,7 +5,18 @@
 # Date: 07.05.2023                   #
 # ********************************** #
 
-import os
 import numpy as np
 
-
+def stack_slice(volume: np.ndarray, image: np.ndarray, slice_idx: int) -> np.ndarray:
+    """
+    Implements and outputs a sliced sinogram, parametrized by the row slicing row index.
+    :param:
+        volume: np.ndarray of np.ndarrays, stack of projections
+        image: np.ndarray, array representation of the image
+        slice_idx: int, slicing row index
+    :return:
+        sliced_image: np.ndarray, sliced image given the slicing position
+    """
+    sliced_row = image[:slice_idx, :]
+    volumne = np.vstack((volume, sliced_row))
+    return volume
