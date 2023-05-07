a7c22513cc54292f6df1ef9e06364327b852980d
ge86yaw <kaanguney.keklikci@tum.de>
2023-05-07 15:04:11 +0200

    chore(repo): initial commit for hw02

---
+++ homework/hw02/slicing.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# ********************************** #
+# Author: kaanguney.keklikci@tum.de  #
+# Date: 07.05.2023                   #
+# ********************************** #
+
+import os
+import numpy as np
+
+
---
+++ tests/test_hw02.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# ********************************** #
+# Author: kaanguney.keklikci@tum.de  #
+# Date: 07.05.2023                   #
+# ********************************** #
+
+from PIL import Image
+import numpy as np
+
+def test_slicing(image: np.ndarray, slice_idx: int = 100) -> np.ndarray:
+    """
+    Implements and outputs a sliced sinogram, parametrized by the row slicing row index.
+    :param:
+        image: np.ndarray, array representation of the image
+        slice_idx: int, slicing row index
+    :return:
+        sliced_image: np.ndarray, sliced image given the slicing position
+    """
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
