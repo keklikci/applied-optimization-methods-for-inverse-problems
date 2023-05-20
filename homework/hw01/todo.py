510f0cad8ed286e1bac290c6c9810ba849796f85
ge86yaw <kaanguney.keklikci@tum.de>
2023-05-20 13:11:49 +0200

    fix(repo): small fixes

---
+++ homework/hw01/todo.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# ********************************** #
+# Author: kaanguney.keklikci@tum.de  #
+# Date: 20.05.2023                   #
+# ********************************** #
+
+# TODO 1: fix (replace) filters using David's implementation
+# TODO 2: fix `backproject.py` (possible) dimension mismatches, i.e., add filter compatibility 
---
+++ homework/hw02/output/fair_potential/todo.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# ********************************** #
+# Author: kaanguney.keklikci@tum.de  #
+# Date: 20.05.2023                   #
+# ********************************** #
+
+# TODO 1: fix fair potential implementation
+# TODO 2: experiment using smaller number of iterations
+# TODO 3: double check gradient calculation across reconstruction algorithms
