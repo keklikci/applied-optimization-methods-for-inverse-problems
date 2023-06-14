cd498bb26b4a2461bc418e4111d616b29d2b059d
ge86yaw <kaanguney.keklikci@tum.de>
2023-06-15 01:39:35 +0200

    feat(repo): add bb1 & bb2 implementation

--- homework/hw04/bb1.py
+++ homework/hw04/bb1.py
@@ -17,5 +17,41 @@ class BB1(LineSearch):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-    def optimize(self) -> None:
-        pass
+    def optimize(self, operator, sino, num_iterations=100) -> None:
+        x = self.x0
+        gradient = self.calculate_gradient(x)
+        prev_gradient = gradient
+        prev_x = x
+        for i in range(num_iterations):
+            # avoid division by zero for the first iteration
+            if not i:
+                step = 1e-5
+            else:
+                gradient_diff = grad - prev_gradient
+                x_diff = x - prev_x
+                step = np.dot(x_diff.T, gradient_diff) / gradient_diff.T * gradient_diff
+            next_x = x - step * gradient
+            error = self.calculate_norm(next_x)
+            objective = self.calculate_norm(x)
+            terminal_bound = error < objective
+            if terminal_bound:
+                break
+            prev_x = x
+            x = next_x
+            prev_gradient = gradient
+            gradient = operator.applyAdjoint(operator.apply(x) - sino)
+        # descent with the optimal step
+        for i in range(num_iterations):
+            x -= step * gradient
+        return x
+
+
+def main():
+    bb1 = BB1()
+    output = bb1.optimize()
+    os.makedirs("images", exist_ok=True)
+    tifffile.imsave("images/bb1.tif", output.astype(np.uint8))
+
+
+if __name__ == "__main__":
+    main()
--- homework/hw04/bb2.py
+++ homework/hw04/bb2.py
@@ -17,4 +17,40 @@ class BB2(LineSearch):
         super().__init__(*args, **kwargs)
 
     def optimize(self) -> None:
-        pass
+        x = self.x0
+        gradient = self.calculate_gradient(x)
+        prev_gradient = gradient
+        prev_x = x
+        for i in range(num_iterations):
+            # avoid division by zero for the first iteration
+            if not i:
+                step = 1e-5
+            else:
+                gradient_diff = grad - prev_gradient
+                x_diff = x - prev_x
+                step = np.dot(x_diff.T, x_diff) / x_diff.T * gradient_diff
+            next_x = x - step * gradient
+            error = self.calculate_norm(next_x)
+            objective = self.calculate_norm(x)
+            terminal_bound = error < objective
+            if terminal_bound:
+                break
+            prev_x = x
+            x = next_x
+            prev_gradient = gradient
+            gradient = operator.applyAdjoint(operator.apply(x) - sino)
+        # descent with the optimal step
+        for i in range(num_iterations):
+            x -= step * gradient
+        return x
+
+
+def main():
+    bb2 = BB2()
+    output = bb2.optimize()
+    os.makedirs("images", exist_ok=True)
+    tifffile.imsave("images/bb2.tif", output.astype(np.uint8))
+
+
+if __name__ == "__main__":
+    main()
