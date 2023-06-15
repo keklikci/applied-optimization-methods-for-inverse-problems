25aa7970809db21c878255fb91d6f4d6d0e99bb4
ge86yaw <kaanguney.keklikci@tum.de>
2023-06-15 02:48:34 +0200

    feat(repo): add projected gradient descent implementation

---
+++ homework/hw04/projected_gradient_descent.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+# ********************************** #
+# Author: kaanguney.keklikci@tum.de  #
+# Date: 14.06.2023                   #
+# ********************************** #
+
+import aomip
+import numpy as np
+import tifffile
+import os
+from optimize import Optimization
+
+
+class ProjectedGradientDescent(Optimization):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.step = 1e-3
+
+    @property
+    def step(self) -> float:
+        return self._step
+
+    @step.setter
+    def step(self, step) -> float:
+        self._step = step
+
+    def project(self, x, gradient) -> np.ndarray:
+        xproj = np.maximum(x - self.step * gradient, 0)
+        return xproj
+
+    def optimize(self, num_iterations=100) -> None:
+        x = self.x0
+        for i in range(num_iterations):
+            gradient = self.calculate_gradient(x)
+            x = self.project(x, gradient)
+        return x
+
+
+def main():
+    descent = ProjectedGradientDescent()
+    output = descent.optimize()
+    os.makedirs("images", exist_ok=True)
+    tifffile.imsave("images/proj_grad_descent.tif", output.astype(np.uint8))
+
+
+if __name__ == "__main__":
+    main()
