c203344543926e8a7c70f8c4b8378301e69be991
ge86yaw <kaanguney.keklikci@tum.de>
2023-06-15 07:39:37 +0200

    fix(repo): small fix

--- homework/hw04/semi_convergence.py
+++ homework/hw04/semi_convergence.py
@@ -10,7 +10,7 @@ import numpy as np
 import tifffile
 import os
 
-# usual parameter setup
+# usual setup
 vol_shape = [512, 512]
 sino_shape = [512]
 d2c = vol_shape[0] * 100.0
@@ -18,11 +18,8 @@ c2d = vol_shape[0] * 5.0
 thetas = np.arange(360)
 phantom = aomip.shepp_logan(vol_shape)
 sino = aomip.radon(phantom, sino_shape, thetas, d2c, c2d)
-
-# define the operator
 operator = aomip.XrayOperator(vol_shape, sino_shape, thetas, d2c, c2d)
 
-# apply filter to the sinogram
 def apply_filter(sino, sino_shape):
     H = np.linspace(-1, 1, sino_shape[0])
     ram_lak = np.abs(H)
@@ -33,13 +30,8 @@ def apply_filter(sino, sino_shape):
     return sino
 
 def experiment(x, operator, sino, sino_shape, alpha=1e-4, num_iterations=100, log_error=True) -> None:
-    # define a callback array
     callback = []
-    # --- noise snippet ---
-    noise = aomip.noise.Noise()
-    noise.update(method="Poisson")
-    sino = noise.transform(sino)
-    # --- noise snippet ---
+    sino = np.random.poisson(sino).astype(np.uint8)
     sino = apply_filter(sino, sino_shape)
     for i in range(num_iterations):
         error = np.linalg.norm(operator.apply(x) - sino)
