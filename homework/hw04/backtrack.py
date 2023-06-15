c3dfd6c2380eed6f273caa87e6cb1a735128cd83
ge86yaw <kaanguney.keklikci@tum.de>
2023-06-15 09:05:00 +0200

    feat(repo): implement convergence analysis for backtrack

--- homework/hw04/backtrack.py
+++ homework/hw04/backtrack.py
@@ -16,7 +16,7 @@ class Backtracking(LineSearch):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-    def optimize(self, alpha=1e-5, beta=0.75, num_iterations=100) -> None:
+    def optimize(self, alpha=1e-5, beta=0.75, num_iterations=100, callback=None) -> None:
         x = self.x0
         step = 1.0
         gradient = self.calculate_gradient(x)
@@ -24,6 +24,8 @@ class Backtracking(LineSearch):
         for i in range(num_iterations):
             next_x = x - step * gradient
             error = self.calculate_norm(next_x)
+            if callback:
+                callback.append(error)
             objective = self.calculate_norm(x)
             terminal_bound = error <= objective - alpha * step * norm
             if terminal_bound:
@@ -33,15 +35,21 @@ class Backtracking(LineSearch):
         # descent with the optimal step
         for i in range(num_iterations):
             x -= step * gradient
-        return x
+        return x, callback
 
 
 def main():
     backtrack = Backtracking()
-    output = backtrack.optimize()
-    os.makedirs("images", exist_ok=True)
-    tifffile.imsave("images/backtrack.tif", output.astype(np.uint8))
-
+    alphas = [1e-3, 1e-4, 1e-5, 1e-6, 1e-7]
+    callback = []
+    for alpha in alphas:
+        x, callback = backtrack.optimize(alpha=alpha, callback=callback)
+        os.makedirs("images", exist_ok=True)
+        tifffile.imsave(f"images/backtrack_{alpha}.tif", x.astype(np.uint8))
+        plt.ylabel(f"Reconstruction error, alpha = {alpha}")
+        plt.xlabel(f"# of iterations")
+        plt.plot(np.arange(len(callback)), callback)
+        plt.savefig(f"images/callback_{alpha}", transparent=True)
 
 if __name__ == "__main__":
     main()
