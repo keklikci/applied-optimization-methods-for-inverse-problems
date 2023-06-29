733d4518fbc0010ac2ec1517ec5360f7d82133ce
ge86yaw <kaanguney.keklikci@tum.de>
2023-06-29 16:31:30 +0200

    fix(repo): fix reconstruction export

--- homework/hw06/config.py
+++ homework/hw06/config.py
@@ -11,12 +11,14 @@ import numpy as np
 config = {}
 
 def configure(data, metadata) -> dict:
-    config["angles"] = np.degrees(np.array(metadata["angles"])[: metadata["rotview"]])
+    # config["angles"] = np.degrees(np.array(metadata["angles"])[: metadata["rotview"]])
+    config["angles"] = np.arange(0, 360, 5)
     config["voxels"] = 0.7
     config["voxscale"] = 1 / config["voxels"]
-    config["volspace"] = [512, 512]
-    config["sinospace"] = [512]
+    config["sinoshape"] = [128, 128]
     config["ndetector"] = data.shape[:-1]
+    config["volspace"] = config["voxscale"]
+    config["volshape"] = data.shape
     config["detectorspace"] = config["voxscale"] * metadata["du"]
     config["ds2c"] = config["voxscale"] * metadata["dso"]
     config["dc2d"] = config["voxscale"] * metadata["ddo"]
--- homework/hw06/reconstruct.py
+++ homework/hw06/reconstruct.py
@@ -18,21 +18,20 @@ qpath = "/srv/ceph/share-all/aomip/mayo_clinical/out/L310_flat_fan_projections_q
 def main():
     fdata, fmeta = load(fpath)
     qdata, qmeta = load(qpath)
-    fconfig, qconfig = configure(fdata, fmeta), config(qdata, qmeta)
-    operator = aomip.XrayOperator(fconfig["volspace"], fconfig["sinospace"], fconfig["angles"], fconfig["ds2c"], fconfig["dc2d"])
-    fsino = aomip.radon(fdata, fconfig["sinospace"], fconfig["angles"], fconfig["ds2c"], fconfig["dc2d"])
-    qsino = aomip.radon(qdata, qconfig["sinospace"], qconfig["angles"], qconfig["ds2c"], qconfig["dc2d"])
+    fconfig, qconfig = configure(fdata, fmeta), configure(qdata, qmeta)
+    operator = aomip.XrayOperator(fconfig["volshape"], fconfig["sinoshape"], fconfig["angles"], fconfig["ds2c"], fconfig["dc2d"])
+    fsino = aomip.radon(fdata, fconfig["sinoshape"], fconfig["angles"], fconfig["ds2c"], fconfig["dc2d"])
+    qsino = aomip.radon(qdata, qconfig["sinoshape"], qconfig["angles"], qconfig["ds2c"], qconfig["dc2d"])
     lr, n = 1e-3, 100
     for i in range(n):
         fgrad = operator.applyAdjoint(operator.apply(fdata) - fsino)
         qgrad = operator.applyAdjoint(operator.apply(qdata) - qsino)
         fdata -= lr * fgrad
         qdata -= lr * qgrad
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (12, 5))
-    ax1.imshow(fdata, cmap="gray")
-    ax2.imshow(qdata, cmap="gray")
+    # fig, (ax1, ax2) = plt.subplots(1, 2, figshape = (12, 5))
     os.makedirs("images", exist_ok=True)
-    plt.savefig("images/reconstruction.png")
+    tifffile.imwrite("images/qrecon.tif", qdata)
+    tifffile.imwrite("images/frecon.tif", fdata)
 
 if __name__ == "__main__":
     main()
