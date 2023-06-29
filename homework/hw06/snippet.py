f0dc346e6c0048cfe79dcaf4ec2826b3d387828a
ge86yaw <kaanguney.keklikci@tum.de>
2023-06-29 11:34:10 +0200

    feat(repo): add code snippet

---
+++ homework/hw06/snippet.py
@@ -0,0 +1,34 @@
+
+from skimage.io import imread, imsave
+import json
+
+def load_tiff_stack_with_metadata(file):
+    if not (file.name.endswith('.tif') or file.name.endswith('.tiff')):
+        raise FileNotFoundError('File has to be tif.')
+    with tifffile.TiffFile(file) as tif:
+        data = tif.asarray()
+        metadata = tif.pages[0].tags["ImageDescription"].value
+    metadata = metadata.replace("'", "\"")
+    try:
+        metadata = json.loads(metadata)
+    except:
+        print('The tiff file you try to open does not seem to have metadata attached.')
+        metadata = None
+    return data, metadata
+
+# extract angles in degree
+angles = np.degrees(np.array(metadata["angles"])[: metadata["rotview"]])
+
+# setup some spacing and sizes
+voxel_size = 0.7 # can be adjusted
+vox_scaling = 1 / voxel_size
+vol_spacing = vox_scaling
+vol_size = image_size
+
+# size of detector
+det_count = sino_data.shape[:-1]
+det_spacing = vox_scaling * metadata["du"]
+
+# Distances from source to center, and center to detector
+ds2c = vox_scaling * metadata["dso"]
+dc2d = vox_scaling * metadata["ddo"]
