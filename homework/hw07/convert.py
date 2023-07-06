c8995030fae166df4cc7801bcced510fb4cac92a
ge86yaw <kaanguney.keklikci@tum.de>
2023-07-06 16:34:40 +0200

    chore(repo): script for notebook export

---
+++ homework/hw07/convert.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# ********************************** #
+# Author: kaanguney.keklikci@tum.de  #
+# Date: 05.07.2023                   #
+# ********************************** #
+
+import os
+import matplotlib.pyplot as plt
+
+def main():
+    images = os.listdir("images")
+    images = [image for image in images if image.endswith(".tif")]
+    for i, image in enumerate(images):
+        filename = os.path.join(os.getcwd(), "images", image)
+        plt.axis("off")
+        export = plt.imread(filename)
+        export = plt.imshow(export, cmap="gray")
+        plt.colorbar(export)
+        plt.tight_layout()
+        idx = filename.find("_")
+        plt.title(f"τ ={filename[(idx + 1):len(filename) - 4]}")
+        plt.savefig(images[i].replace(".tif", ".png"))
+        plt.clf()
+
+if __name__ == "__main__":
+    main()
