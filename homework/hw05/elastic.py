642fa8930c094b9e3485fba1158b163e44404253
ge86yaw <kaanguney.keklikci@tum.de>
2023-07-10 14:57:46 +0200

    chore(repo): update elastic net with correct proximal operator

--- homework/hw05/elastic.py
+++ homework/hw05/elastic.py
@@ -10,58 +10,50 @@ import numpy as np
 import tifffile
 import os
 import matplotlib.pyplot as plt
-from optimize import Optimization
-from proximal_operators.nonnegativity import Nonnegativity
-from proximal_operators.l2 import L2
 
 
 class Elastic(Optimization):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.step = 1e-3
+        self.lmbd = 1.0
+        self.f = aomip.ElasticNet()
 
-    @property
-    def step(self) -> float:
-        return self._step
-
-    @step.setter
-    def step(self, step) -> float:
-        self._step = step
-
-    def optimize(self, num_iterations=100, callback=None) -> None:
+    def optimize(self, n=100) -> np.ndarray:
         x, z = self.x0, self.x0
         t = 1
-        for i in range(num_iterations):
+        for _ in range(n):
             xprev, zprev = x, z
             gradient = self.calculate_gradient(z)
-            # add l1, l2 terms to the gradient
-            gradient += (self.step * np.sign(zprev)) + (self.step * zprev)
             tprev = t
-            t = (1 + np.sqrt(1 + 4 * t**2)) / 2
+            t = (1 + np.sqrt(1 + 4 * t ** 2)) / 2
             # calculate momentum parameter
-            self.step = (tprev - 1) / t
-            z = Nonnegativity().proximal(xprev, self.step, gradient)
-            x = z + self.step * (z - zprev)
-            if callback is not None and i % 2 == 0:
-                error = self.calculate_norm(x)
-                callback.append(error)
-        return x, callback
+            self.lmbd = (tprev - 1) / t
+            # enforce a combination of l1 and l2 regularization
+            z = self.f.proximal(xprev - 1 / L * gradient, lmbd=(1 / L))
+            x = z + self.lmbd * (z - zprev)
+        return x
 
 
 def main():
     elastic = Elastic()
-    alphas = np.linspace(1e-5, 1e-3, num=5)
-    for i, alpha in enumerate(alphas):
-        callback = []
-        elastic.step = alpha
-        x, callback = elastic.optimize(callback=callback)
-        os.makedirs("images", exist_ok=True)
-        tifffile.imsave(f"images/elastic_proximal_{i + 1}.tif", x.astype(np.uint8))
-        plt.plot(callback)
-        plt.ylabel(f"Reconstruction error, alpha = {alpha}")
-        plt.xlabel(f"# of iterations")
-        plt.savefig(f"images/elastic_proximal_callback_{i + 1}")
+    os.makedirs("images/notebook/elastic", exist_ok=True)
+    os.makedirs("images/elastic", exist_ok=True)
+    lambdas = np.logspace(-3, 6, 10)
+    for lmbd in lambdas:
+        elastic.lmbd = lmbd
+        x = elastic.optimize()
+        # scale output
+        x *= 1e3
+        # save notebook output
+        plt.axis("off")
+        export = plt.imshow(x, cmap="gray")
+        plt.colorbar(export)
+        plt.tight_layout()
+        plt.title(f"λ ={lmbd}")
+        plt.savefig(f"images/notebook/elastic/lambda_{lmbd}.png")
         plt.clf()
+        # save tif output
+        tifffile.imwrite(f"images/elastic/lambda_{lmbd}.tif", x)
 
 
 if __name__ == "__main__":
