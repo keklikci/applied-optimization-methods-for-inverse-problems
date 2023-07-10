139ca406eda9798dbba91f780017c88944613346
ge86yaw <kaanguney.keklikci@tum.de>
2023-07-10 15:36:37 +0200

    chore(repo): export elastic net regularization outputs

--- homework/hw05/elastic.py
+++ homework/hw05/elastic.py
@@ -12,11 +12,12 @@ import os
 import matplotlib.pyplot as plt
 
 
-class Elastic(Optimization):
+class Elastic(aomip.Optimization):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.lmbd = 1.0
         self.f = aomip.ElasticNet()
+        self.g = aomip.Nonnegativity()
 
     def optimize(self, n=100) -> np.ndarray:
         x, z = self.x0, self.x0
@@ -24,13 +25,16 @@ class Elastic(Optimization):
         for _ in range(n):
             xprev, zprev = x, z
             gradient = self.calculate_gradient(z)
+            L = np.linalg.norm(gradient, ord=2)
             tprev = t
             t = (1 + np.sqrt(1 + 4 * t ** 2)) / 2
             # calculate momentum parameter
             self.lmbd = (tprev - 1) / t
             # enforce a combination of l1 and l2 regularization
-            z = self.f.proximal(xprev - 1 / L * gradient, lmbd=(1 / L))
+            z = self.f.proximal(xprev - 1 / L * gradient, lmbd=(1 / L), sigma=1.0)
             x = z + self.lmbd * (z - zprev)
+        # return nonnegative
+        x = self.g.proximal(x)
         return x
 
 
@@ -42,8 +46,6 @@ def main():
     for lmbd in lambdas:
         elastic.lmbd = lmbd
         x = elastic.optimize()
-        # scale output
-        x *= 1e3
         # save notebook output
         plt.axis("off")
         export = plt.imshow(x, cmap="gray")
