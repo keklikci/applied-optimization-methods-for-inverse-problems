195a9a7fe3fafae70b393cd8c8238604fc0b4dc4
ge86yaw <kaanguney.keklikci@tum.de>
2023-07-10 19:03:29 +0200

    chore(repo): export bb1 outputs

--- homework/hw04/bb1.py
+++ homework/hw04/bb1.py
@@ -7,10 +7,11 @@
 
 import numpy as np
 import tifffile
+import matplotlib.pyplot as plt
 import os
 import aomip
 
-class BB1(Optimization):
+class BB1(aomip.Optimization):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
@@ -24,7 +25,7 @@ class BB1(Optimization):
             if not i:
                 step = lmbd
             else:
-                gradient_diff = grad - prev_gradient
+                gradient_diff = gradient - prev_gradient
                 x_diff = x - prev_x
                 step = np.dot(x_diff.T, gradient_diff) / gradient_diff.T * gradient_diff
             next_x = x - step * gradient
@@ -47,7 +48,7 @@ def main():
     bb1 = BB1()
     os.makedirs("images/notebook/bb1", exist_ok=True)
     os.makedirs("images/bb1", exist_ok=True)
-    lambdas = [1e-4, 1e-3, 1e-2, 1e-1]
+    lambdas = [1e-6, 1e-5, 1e-4, 1e-3]
     for lmbd in lambdas:
         x = bb1.optimize(lmbd=lmbd)
         # save notebook output
@@ -55,11 +56,11 @@ def main():
         export = plt.imshow(x, cmap="gray")
         plt.colorbar(export)
         plt.tight_layout()
-        plt.title(f"α={alpha}")
+        plt.title(f"λ ={lmbd}")
         plt.savefig(f"images/notebook/bb1/lambda_{lmbd}.png")
         plt.clf()
         # save tif output
-        tifffile.imwrite(f"images/bb1/bb1/lambda_{lmbd}.tif", x)
+        tifffile.imwrite(f"images/bb1/lambda_{lmbd}.tif", x)
 
 
 if __name__ == "__main__":
