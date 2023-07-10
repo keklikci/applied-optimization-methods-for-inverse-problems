851afbd4f97be32570fb103492c884bcc9f4cbb7
ge86yaw <kaanguney.keklikci@tum.de>
2023-07-10 18:57:57 +0200

    chore(repo): start refinement of BB1

--- homework/hw04/bb1.py
+++ homework/hw04/bb1.py
@@ -8,24 +8,21 @@
 import numpy as np
 import tifffile
 import os
-import sys
 import aomip
-from optimize import Optimization
-
 
 class BB1(Optimization):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-    def optimize(self, num_iterations=100) -> None:
+    def optimize(self, n=100, lmbd=1e-3) -> np.ndarray:
         x = self.x0
         gradient = self.calculate_gradient(x)
         prev_gradient = gradient
         prev_x = x
-        for i in range(num_iterations):
+        for i in range(n):
             # avoid division by zero for the first iteration
             if not i:
-                step = 1e-3
+                step = lmbd
             else:
                 gradient_diff = grad - prev_gradient
                 x_diff = x - prev_x
@@ -40,17 +37,29 @@ class BB1(Optimization):
             x = next_x
             prev_gradient = gradient
             gradient = self.calculate_gradient(x)
-        # descent with the optimal step
-        for i in range(num_iterations):
+        # descent with optimal step
+        for _ in range(n):
             x -= step * gradient
         return x
 
 
 def main():
     bb1 = BB1()
-    x = bb1.optimize()
-    os.makedirs("images", exist_ok=True)
-    tifffile.imsave("images/bb1.tif", x.astype(np.uint8))
+    os.makedirs("images/notebook/bb1", exist_ok=True)
+    os.makedirs("images/bb1", exist_ok=True)
+    lambdas = [1e-4, 1e-3, 1e-2, 1e-1]
+    for lmbd in lambdas:
+        x = bb1.optimize(lmbd=lmbd)
+        # save notebook output
+        plt.axis("off")
+        export = plt.imshow(x, cmap="gray")
+        plt.colorbar(export)
+        plt.tight_layout()
+        plt.title(f"α={alpha}")
+        plt.savefig(f"images/notebook/bb1/lambda_{lmbd}.png")
+        plt.clf()
+        # save tif output
+        tifffile.imwrite(f"images/bb1/bb1/lambda_{lmbd}.tif", x)
 
 
 if __name__ == "__main__":
