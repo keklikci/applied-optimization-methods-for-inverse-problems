b3c9d42b9e9a72346349dfebf50408613e21ee6a
ge86yaw <kaanguney.keklikci@tum.de>
2023-07-10 18:27:33 +0200

    chore(repo): start backtrack refinement

--- homework/hw04/backtrack.py
+++ homework/hw04/backtrack.py
@@ -9,47 +9,50 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 import tifffile
-from optimize import Optimization
+import aomip
 
 
-class Backtracking(Optimization):
+class Backtracking(aomip.Optimization):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-    def optimize(self, alpha=1e-5, beta=0.9, num_iterations=100, callback=None) -> None:
+    def optimize(self, alpha=0.1, beta=0.5, n=100) -> np.ndarray:
         x = self.x0
         step = 1.0
         gradient = self.calculate_gradient(x)
         norm = np.linalg.norm(gradient)
-        for i in range(num_iterations):
+        for _ in range(n):
             next_x = x - step * gradient
             error = self.calculate_norm(next_x)
-            if callback is not None and i % 2 == 0:
-                callback.append(error)
             objective = self.calculate_norm(x)
             terminal_bound = error <= objective - alpha * step * norm
             if terminal_bound:
                 break
             else:
                 step = self.decay(step, beta)
-        # descent with the optimal step
-        for i in range(num_iterations):
+        # descent with optimal step
+        for _ in range(n):
             x -= step * gradient
-        return x, callback
+        return x
 
 
 def main():
     backtrack = Backtracking()
-    alphas = np.linspace(0.0075, 0.011, num=5)
-    callback = []
-    for i, alpha in enumerate(alphas):
-        x, callback = backtrack.optimize(alpha=alpha, callback=callback)
-        os.makedirs("images", exist_ok=True)
-        tifffile.imsave(f"images/backtrack_{i + 1}.tif", x.astype(np.uint8))
-        plt.plot(np.arange(len(callback)), callback)
-        plt.ylabel(f"Reconstruction error, alpha = {alpha}")
-        plt.xlabel(f"# of iterations")
-        plt.savefig(f"images/backtrack_callback_{i + 1}")
+    os.makedirs("images/notebook", exist_ok=True)
+    os.makedirs("images/backtrack", exist_ok=True)
+    alphas = np.linspace(0, 1, num=5)
+    for alpha in alphas:
+        x = backtrack.optimize(alpha=alpha)
+        # save notebook output
+        plt.axis("off")
+        export = plt.imshow(x, cmap="gray")
+        plt.colorbar(export)
+        plt.tight_layout()
+        plt.title(f"α={alpha}")
+        plt.savefig(f"images/notebook/backtrack/alpha_{alpha}_beta_{0.5}.png")
+        plt.clf()
+        # save tif output
+        tifffile.imwrite(f"images/backtrack/alpha_{alpha}_beta_{0.5}.tif", x)
 
 
 if __name__ == "__main__":
