848cba28b7b136ae1424df7b6a125dbe3d03c031
ge86yaw <kaanguney.keklikci@tum.de>
2023-07-10 20:11:23 +0200

    chore(repo): update BB2

--- homework/hw04/bb2.py
+++ homework/hw04/bb2.py
@@ -7,26 +7,25 @@
 
 import numpy as np
 import tifffile
+import matplotlib.pyplot as plt
 import os
-import sys
-from optimize import Optimization
+import aomip
 
-
-class BB2(Optimization):
+class BB2(aomip.Optimization):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-    def optimize(self, num_iterations=100) -> None:
+    def optimize(self, n=100, lmbd=1e-3) -> np.ndarray:
         x = self.x0
         gradient = self.calculate_gradient(x)
         prev_gradient = gradient
         prev_x = x
-        for i in range(num_iterations):
+        for i in range(n):
             # avoid division by zero for the first iteration
             if not i:
-                step = 1e-3
+                step = lmbd
             else:
-                gradient_diff = grad - prev_gradient
+                gradient_diff = gradient - prev_gradient
                 x_diff = x - prev_x
                 step = np.dot(x_diff.T, x_diff) / x_diff.T * gradient_diff
             next_x = x - step * gradient
@@ -39,17 +38,29 @@ class BB2(Optimization):
             x = next_x
             prev_gradient = gradient
             gradient = self.calculate_gradient(x)
-        # descent with the optimal step
-        for i in range(num_iterations):
+        # descent with optimal step
+        for _ in range(n):
             x -= step * gradient
         return x
 
 
 def main():
     bb2 = BB2()
-    x = bb2.optimize()
-    os.makedirs("images", exist_ok=True)
-    tifffile.imsave("images/bb2.tif", x.astype(np.uint8))
+    os.makedirs("images/notebook/bb2", exist_ok=True)
+    os.makedirs("images/bb2", exist_ok=True)
+    lambdas = [1e-6, 1e-5, 1e-4, 1e-3]
+    for lmbd in lambdas:
+        x = bb1.optimize(lmbd=lmbd)
+        # save notebook output
+        plt.axis("off")
+        export = plt.imshow(x, cmap="gray")
+        plt.colorbar(export)
+        plt.tight_layout()
+        plt.title(f"λ ={lmbd}")
+        plt.savefig(f"images/notebook/bb2/lambda_{lmbd}.png")
+        plt.clf()
+        # save tif output
+        tifffile.imwrite(f"images/bb2/lambda_{lmbd}.tif", x)
 
 
 if __name__ == "__main__":
