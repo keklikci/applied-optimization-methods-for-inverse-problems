9edbb43af2156ce191670c98929558427bf5d236
ge86yaw <kaanguney.keklikci@tum.de>
2023-07-12 19:41:09 +0200

    chore(repo): parameter update to gd

--- homework/hw08/gd.py
+++ homework/hw08/gd.py
@@ -16,7 +16,7 @@ def main():
     os.makedirs("images/notebook/gd/convergence", exist_ok=True)
     os.makedirs("images/gd", exist_ok=True)
     gd = aomip.GradientDescent()
-    lrs = [1e-7, 1e-8, 1e-6, 1e-5]
+    lrs = [1e-5, 1e-4, 1e-3]
     for lr in lrs:
         x, history = gd.optimize(lr=lr)
         # save notebook output
